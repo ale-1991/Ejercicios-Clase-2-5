9434c4e9ab90bf87b26d2261e85596b150df3ce8
Alejandro Caro <alecaro014@gmail.com>
2023-03-28 17:02:36 -0300

    mensaje de confirmación

--- src/vectoresymatrices/vectoresymatricesejercicios/vectoresymatricesejerciciosextras/ejercicioextra4.java
+++ src/vectoresymatrices/vectoresymatricesejercicios/vectoresymatricesejerciciosextras/ejercicioextra4.java
@@ -16,10 +16,52 @@ notas del curso.
  */
 package vectoresymatrices.vectoresymatricesejercicios.vectoresymatricesejerciciosextras;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Alejandro Caro
  */
 public class ejercicioextra4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int alumnos = 10;
+        int aprobados=0;
+        int desaprobados=0;
+        double suma = 0;
+        double aux =0;
+        double[] notasFinales = new double [alumnos];
+        
+        for (int i=0;i<alumnos;i++){
+            System.out.println("Ingrese la nota del primer trabajo practico del alumno "+ (i+1) +".");
+            int nota1 = sc.nextInt();
+            System.out.println("Ingrese la nota del segundo trabajo practico del alumno "+ (i+1) +".");
+            int nota2 = sc.nextInt();
+            System.out.println("Ingrese la nota del primer integrador del alumno "+(i+1)+".");
+            int nota3 = sc.nextInt();
+            System.out.println("Ingrese la nota del segundo integrador del alumno "+(i+1)+".");
+            int nota4 = sc.nextInt();
+            double promedio = (nota1*0.1+nota2*0.15+nota3*0.25+nota4*0.50);
+            
+            System.out.println("***Promedio del alumno " +(i+1)+" :" +promedio+".***");
+            if(promedio>=7){
+                aprobados++;
+            }else{
+                desaprobados++;
+            }
+            notasFinales[i]= promedio;
+            
+        }
+        System.out.println("---NOTAS FINALES---");
+        for(int i=0;i<alumnos;i++){
+            System.out.print("["+notasFinales[i]+"]");
+        }
+        System.out.println("");
+        
+        System.out.println("La cantidad de alumnos aprobados es de: " + aprobados+".");
+        System.out.println("La cantidad de alumnos desaprobados es de: "+desaprobados+".");
+ 
+    }
+  
     
 }
